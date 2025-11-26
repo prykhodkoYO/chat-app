ff996d5baadba28066366f58521fda740ecdd86b
EgorPrihodko123 <gestithe@gmail.com>
2025-11-26 06:44:56 +0200

    Configure Husky & lint-staged; apply Prettier auto-format; fix trailing spaces and style issues

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,35 +1,34 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+
   {
-    files: ["**/*.{js,ts,jsx,tsx}"],
-    ignores: ["node_modules", "dist", "build"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-      parser: tseslint.parser,
-      parserOptions: {
-        project: "./tsconfig.json",
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+      'react-refresh/only-export-components': 'warn',
+      'react/react-in-jsx-scope': 'off',
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      prettier,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-
-      "react-refresh/only-export-components": "warn",
-      "prettier/prettier": "error",
-    },
   },
-  prettierConfig,
 ];
