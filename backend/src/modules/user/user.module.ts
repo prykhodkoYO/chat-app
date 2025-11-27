bd8d47e8eb64ca8309b711cb07d5570f4b32d133
EgorPrihodko123 <gestithe@gmail.com>
2025-11-27 21:42:55 +0200

    fix: register CloudinaryModule in UserModule to resolve missing dependency

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), CloudinaryModule],
   controllers: [UserController],
   providers: [UserService],
 })
