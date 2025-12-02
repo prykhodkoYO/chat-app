532a8ef84fd5175808f2ff907a00fddeb0048aad
EgorPrihodko123 <gestithe@gmail.com>
2025-12-02 10:46:54 +0200

    fix(cloudinary): resolve circular type issue & fix DI injection

--- backend/src/cloudinary/cloudinary.service.ts
+++ backend/src/cloudinary/cloudinary.service.ts
@@ -1,18 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { v2 as cloudinary } from 'cloudinary';
+import { Inject, Injectable } from '@nestjs/common';
 import { UploadApiResponse } from 'cloudinary';
 
 @Injectable()
 export class CloudinaryService {
+  constructor(
+    @Inject('CLOUDINARY')
+    private cloudinary: any,
+  ) {}
+
   async uploadImage(file: Express.Multer.File): Promise<string> {
     return new Promise((resolve, reject) => {
-      const upload = cloudinary.uploader.upload_stream(
+      const upload = this.cloudinary.uploader.upload_stream(
         { folder: 'avatars' },
-        (err, result: UploadApiResponse | undefined) => {
+        (err: any, result: UploadApiResponse | undefined) => {
           if (err || !result) return reject(err);
           resolve(result.secure_url);
-        }
+        },
       );
+
       upload.end(file.buffer);
     });
   }
--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -4,9 +4,10 @@ import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]),CloudinaryModule],
   controllers: [UserController],
   providers: [UserService,CloudinaryService],
 })
